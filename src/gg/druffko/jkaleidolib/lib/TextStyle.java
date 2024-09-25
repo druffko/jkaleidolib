ba9535514253994eb8c9fb61b4ed2e10289c3415
druffko <christoph@rustige.me>
2024-09-25 11:17:16 +0200

    Implemented TextStyles

---
+++ src/gg/druffko/jkaleidolib/lib/TextStyle.java
@@ -0,0 +1,23 @@
+package gg.druffko.jkaleidolib.lib;
+
+public class TextStyle {
+
+    // Text Styles
+    public static final String reset = "\u001B[0m";
+    public static final String bold = "\u001B[1m";
+    public static final String italic = "\u001B[3m";
+    public static final String dim = "\u001B[2m";
+    public static final String slowblink = "\u001B[5m"; // might not be supported in your terminal
+    public static final String fastblink = "\u001B[6m"; // rarely supported
+    public static final String reverse = "\u001B[7m";
+    public static final String invisible = "\u001B[8m"; // still able to be copied
+
+
+    // Text Decorations
+    public static final String underline = "\u001B[4m";
+    public static final String thickunderline = "\u001B[21m";
+    public static final String italicunderline = "\u001B[4:3m";
+    public static final String strikethrough = "\u001B[9m";
+    public static final String overline = "\u001B[53m";
+
+}
--- src/gg/druffko/jkaleidolib/test/Main.java
+++ src/gg/druffko/jkaleidolib/test/Main.java
@@ -2,6 +2,7 @@ package gg.druffko.jkaleidolib.test;
 
 import gg.druffko.jkaleidolib.lib.BackgroundColor;
 import gg.druffko.jkaleidolib.lib.TextColor;
+import gg.druffko.jkaleidolib.lib.TextStyle;
 
 public class Main {
     public static void main (String[]args){
@@ -24,5 +25,18 @@ public class Main {
         System.out.println(BackgroundColor.cyan + "Cyan" + TextColor.reset);
         System.out.println(BackgroundColor.white + "White" + TextColor.reset);
         System.out.println("Default");
+
+        System.out.println(TextStyle.bold + "Bold Text" + TextStyle.reset);
+        System.out.println(TextStyle.italic + "Italic Text" + TextColor.reset);
+        System.out.println(TextStyle.dim + "Dim Text" + TextColor.reset);
+        System.out.println(TextStyle.slowblink + "Slow Blinking text" + TextColor.reset);
+        System.out.println(TextStyle.fastblink + "Fast Blinking text" + TextColor.reset);
+        System.out.println(TextStyle.reverse + "Reversed Text" + TextColor.reset);
+        System.out.println(TextStyle.invisible + "Invisible Text" + TextColor.reset);
+        System.out.println(TextStyle.underline + "Underlined Text" + TextColor.reset);
+        System.out.println(TextStyle.thickunderline + "D-Underlined Text" + TextColor.reset);
+        System.out.println(TextStyle.italicunderline + "C-Underlined Text" + TextColor.reset);
+        System.out.println(TextStyle.strikethrough + "Crossed out Text" + TextColor.reset);
+        System.out.println(TextStyle.overline + "Overlined Text" + TextColor.reset);
     }
 }
